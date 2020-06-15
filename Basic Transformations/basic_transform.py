a54d009c32dae014c2535891e65581005883162c
BhanuPrakashNani <p.bhanuprakash12345@gmail.com>
2020-06-16 00:08:58 +0530

    Add images

--- Basic Transformations/basic_transform.py
+++ Basic Transformations/basic_transform.py
@@ -1,41 +1,22 @@
-import cv2
-import numpy as np
+import cv2 
+import numpy as np 
 
-img = cv2.imread('calvinHobbes.jpeg')
-(h,w) = img.shape[:2]
-center = (w/2, h/2)
-angle30 = 30
-scale = 1.0
-
-M = cv2.getRotationMatrix2D(center, angle30, scale)
+image = cv2.imread('calvinHobbes.jpeg') 
 
 
-abs_cos = abs(M[0,0]) 
-abs_sin = abs(M[0,1])
+height, width = image.shape[:2] 
+quarter_height, quarter_width = height/4, width/4
 
+T = np.float32([[1, 0, quarter_width], [0, 1, quarter_height]]) 
+img_translation = cv2.warpAffine(image, T, (width, height)) 
 
-bound_w = int(h * abs_sin + w * abs_cos)
-bound_h = int(h * abs_cos + w * abs_sin)
+cv2.imshow("Originalimage", image) 
+cv2.imshow('Translation', img_translation)
+cv2.imwrite('Translation.jpg', img_translation) 
+cv2.waitKey() 
 
-M[0, 2] += bound_w/2 - center[0]
-M[1, 2] += bound_h/2 - center[1]
+cv2.destroyAllWindows() 
 
-rotated30 = cv2.warpAffine(img, M, (bound_w,bound_h))
-
-# cv2.imshow('Original Image', img)
-# cv2.imshow('Rotated by 30', rotated30)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
-
-
-scale_percent = 250
-width = int(img.shape[1] * scale_percent / 100)
-height = int(img.shape[0] * scale_percent / 100)
-dim = (width, height)
-
-scaled = cv2.resize(img, dim, interpolation=cv2.INTER_NEAREST)
-scaled1 = cv2.resize(img, dim, interpolation=cv2.INTER_LINEAR)
-scaled2 = cv2.resize(img, dim, interpolation=cv2.INTER_CUBIC)
 
 # cv2.imshow("Nearest Neighbour", scaled)
 # cv2.waitKey(0)
@@ -48,16 +29,3 @@ scaled2 = cv2.resize(img, dim, interpolation=cv2.INTER_CUBIC)
 # cv2.imshow("Bicubic", scaled)
 # cv2.waitKey(0)
 # cv2.destroyAllWindows()
-
-hheight, qwidth = height/2, width/4
-  
-T = np.float32([[1, 0, qwidth], [0, 1, hheight]]) 
-  
-# We use warpAffine to transform 
-# the image using the matrix, T 
-img_translation = cv2.warpAffine(img, T, (w,h)) 
- 
-cv2.imshow('Translation', img_translation) 
-cv2.waitKey() 
-  
-cv2.destroyAllWindows() 
