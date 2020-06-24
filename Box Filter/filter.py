57a32e0554f64c8922e6d7b0a4b34023481c8c54
BhanuPrakashNani <p.bhanuprakash12345@gmail.com>
2020-06-24 11:05:38 +0530

    Add images

--- Box Filter/filter.py
+++ Box Filter/filter.py
@@ -1,15 +1,32 @@
+# import cv2
+# import numpy as np
+# from matplotlib import pyplot as plt
+
+# img = cv2.imread('panda1.jpg',0)
+# x = int(input())
+
+# kernel = np.ones((x,x),np.float32)/pow(x,2)
+# dst = cv2.blur(img,0,kernel,img,(-1,-1),False,cv2.BORDER_DEFAULT)
+# #cv2.imwrite('dts.png', dst)
+# plt.subplot(121),plt.imshow(img),plt.title('Original')
+# plt.xticks([]), plt.yticks([])
+# plt.subplot(122),plt.imshow(dst),plt.title('Averaging')
+# plt.xticks([]), plt.yticks([])
+# plt.show()
+
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-img = cv2.imread('panda1.jpg',0)
-x = int(input())
+img = cv2.imread('panda1.jpg')
 
-kernel = np.ones((x,x),np.float32)/pow(x,2)
-dst = cv2.boxFilter(img,0,(x,x),img,(-1,-1),False,cv2.BORDER_DEFAULT)
+blur = cv2.blur(img,(5,5))
 
-plt.subplot(121),plt.imshow(img),plt.title('Original')
+plt.imshow(img),plt.title('Original')
+plt.show()
+cv2.imwrite('orig.png', img)
 plt.xticks([]), plt.yticks([])
-plt.subplot(122),plt.imshow(dst),plt.title('Averaging')
+plt.imshow(blur),plt.title('Blurred')
 plt.xticks([]), plt.yticks([])
 plt.show()
+cv2.imwrite('boxfil.png', blur)
